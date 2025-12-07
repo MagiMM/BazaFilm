3c2c2e6d53faf18b36659f2c74c08d1d700fac36
MagMar <marcin@MacBook-Air-MagMar.local>
2025-12-07 10:19:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function addMovie() {
+    var movieTitle  = document.getElementById("inputTitle").value;
+    var movieYear   = document.getElementById("inputYear").value;
+    var movieActors = document.getElementById("inputActors").value;
+
+    console.log(movieTitle);
+    console.log(movieYear);
+    console.log(movieActors);
+
+    if (!movieTitle || !movieYear || !movieActors) {
+        alert("Uzupełnij wszystkie pola!");
+        return;
+    }
+
+    var li = document.createElement("li");
+    li.className = "movie-item";
+    li.innerHTML = "<label><input type='checkbox'>"
+                   + movieTitle + ", " + movieYear + ", " + movieActors + "</label>";
+
+    document.getElementById("moviesList").appendChild(li);
+
+    document.getElementById("inputTitle").value  = "";
+    document.getElementById("inputYear").value   = "";
+    document.getElementById("inputActors").value = "";
+}
+
+function deleteMovies() {
+    var checkedBoxes = document.querySelectorAll("input[type='checkbox']:checked");
+    for (var i = 0; i < checkedBoxes.length; i++) {
+        document.getElementById("moviesList")
+                .removeChild(checkedBoxes[i].parentNode.parentNode);
+    }
+}
